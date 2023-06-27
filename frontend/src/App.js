e2bf777ea402a894c849284598447a486b293c06
Mogan <106959238+Mrdotov@users.noreply.github.com>
2023-06-27 23:42:40 +0300

    Added new file to src

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import Header from './component/header';
+// import NewsImage from '/img/News.jpg';
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <Header />
+         <div className='Posts'>
+            <div className='post-container'>
+              <div className='post'>
+                <img src="/img/News.jpg" alt="News Image" />
+                <h4>Trump heard on tape discussing 'highly confidential, secret' documents</h4>
+                <p>CNN was the first to publish the roughly two-minute recording, which it said came from a July 2021 meeting at Mr Trump's Bedminster, New Jersey golf club between him and several people working on the memoir of his former chief of staff Mark Meadows.</p>
+                <p>12.06.2023</p>
+              </div>
+              <div className='post'>
+                <img src="/img/News.jpg" alt="News Image" />
+                <h4>Trump heard on tape discussing 'highly confidential, secret' documents</h4>
+                <p>CNN was the first to publish the roughly two-minute recording, which it said came from a July 2021 meeting at Mr Trump's Bedminster, New Jersey golf club between him and several people working on the memoir of his former chief of staff Mark Meadows.</p>
+                <p>12.06.2023</p>
+              </div>
+              <div className='post'>
+                <img src="/img/News.jpg" alt="News Image" />
+                <h4>Trump heard on tape discussing 'highly confidential, secret' documents</h4>
+                <p>CNN was the first to publish the roughly two-minute recording, which it said came from a July 2021 meeting at Mr Trump's Bedminster, New Jersey golf club between him and several people working on the memoir of his former chief of staff Mark Meadows.</p>
+                <p>12.06.2023</p>
+              </div>
+              <div className='post'>
+                <img src="/img/News.jpg" alt="News Image" />
+                <h4>Trump heard on tape discussing 'highly confidential, secret' documents</h4>
+                <p>CNN was the first to publish the roughly two-minute recording, which it said came from a July 2021 meeting at Mr Trump's Bedminster, New Jersey golf club between him and several people working on the memoir of his former chief of staff Mark Meadows.</p>
+                <p>12.06.2023</p>
+              </div><div className='post'>
+                <img src="/img/News.jpg" alt="News Image" />
+                <h4>Trump heard on tape discussing 'highly confidential, secret' documents</h4>
+                <p>CNN was the first to publish the roughly two-minute recording, which it said came from a July 2021 meeting at Mr Trump's Bedminster, New Jersey golf club between him and several people working on the memoir of his former chief of staff Mark Meadows.</p>
+                <p>12.06.2023</p>
+              </div>
+
+              
+               </div>
+            </div>
+          </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+      <header className="App-header">
+        <h1>Pozin</h1>
+  
+        <div className='authorization'>
+          <div className='LogIn'>
+            <p>Log In</p>
+          </div>
+          <div className='SignIn'>
+            <p>Sign In</p>
+          </div>
+        </div>
+      </header>
+    );
+  }
